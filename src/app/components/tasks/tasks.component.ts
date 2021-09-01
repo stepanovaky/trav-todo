ce40132c315da7669db5c30a4c51a524599d2e9a
Katerina Stepanova <kstepanova@thelabnyc.com>
2021-09-01 12:26:24 -0400

    click issue

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {TaskService} from '../../services/task.service'
+import { Task } from '../../Tasks'
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [  ];
+
+  constructor(private taskService:TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
+  }
+
+  toggleReminder(task: Task) {
+    // task.reminder = !task.reminder;
+    console.log(task.reminder)
+  }
+
+  deleteTask(task:Task) {
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id))
+  }
+
+} 
